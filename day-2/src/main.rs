53f321b941015f02a5e4a84c4004dadd06f3f94e
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-08 06:29:44 -0500

    day-2 start defining struct

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,13 +2,27 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-struct Password{
+struct Password {
     text: String,
-    rule: String
+    rule: String,
+}
+
+impl Password {
+    fn new(line: &str) -> Password {
+        let password = Password {
+            text: "text".to_string(),
+            rule: "rule".to_string(),
+        };
+        password
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    if let Some(lines) = read_lines("input.txt") {
+        for line in lines {
+            let password = Password::new(&line);
+        }
+    }
 }
 
 fn read_lines<P>(filename: P) -> Option<Vec<String>>
@@ -17,9 +31,5 @@ where
 {
     let file = File::open(filename).ok()?;
     let lines = io::BufReader::new(file).lines();
-    Some(
-        lines
-            .filter_map(|x| x.ok())
-            .collect()
-    )
+    Some(lines.filter_map(|x| x.ok()).collect())
 }
