3d0ea8b9cc4fb4357552786d72461c5da8e3f7b5
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-08 06:27:05 -0500

    day-2 add read_lines

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+struct Password{
+    text: String,
+    rule: String
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn read_lines<P>(filename: P) -> Option<Vec<String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename).ok()?;
+    let lines = io::BufReader::new(file).lines();
+    Some(
+        lines
+            .filter_map(|x| x.ok())
+            .collect()
+    )
 }
