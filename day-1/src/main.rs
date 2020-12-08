33080c16b3d9edc3812d0b4925252dbfbd37d252
Kenny Baskett <kbaskett248@gmail.com>
2020-12-07 19:59:14 -0500

    Successfully readlines

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -12,15 +12,15 @@ fn main() {
     }
 }
 
-fn read_lines<P>(filename: P) -> Result<Vec<i16>, >
+fn read_lines<P>(filename: P) -> Option<Vec<i16>>
 where P: AsRef<Path>, {
-    let file = File::open(filename)?;
+    let file = File::open(filename).ok()?;
     let lines = io::BufReader::new(file).lines();
     Some(
         lines.filter_map(
             |x| {
                 if let Ok(line) = x {
-                    Some(line.parse::<i16>().unwrap())
+                    line.parse::<i16>().ok()
                 } else {
                     None
                 }
