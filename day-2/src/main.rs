ac27685f067638276ec1536bf742c8028a04debd
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-08 19:13:17 -0500

    day 2 part 1

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -5,7 +6,9 @@ use std::path::Path;
 
 struct Password {
     text: String,
-    rule: String,
+    letter: char,
+    min: i8,
+    max: i8,
 }
 
 impl Password {
@@ -13,26 +16,45 @@ impl Password {
         let mut i = line.split(':');
         let rule = i.next()?.trim();
         let text = i.next()?.trim();
+        let mut j = rule.split(' ');
+        let numbers = j.next()?.trim();
+        let letter: char = j.next()?.trim().chars().next()?;
+        let mut k = numbers.split('-');
+        let min: i8 = k.next()?.parse::<i8>().ok()?;
+        let max: i8 = k.next()?.parse::<i8>().ok()?;
         Some(Password {
             text: (*text).to_string(),
-            rule: (*rule).to_string(),
+            letter: letter,
+            min: min,
+            max: max,
         })
+    }
+
+    fn valid_part_1(&self) -> bool {
+        let char_list: Vec<char> = self.text.chars().filter(|x| *x == self.letter).collect();
+        let length: i8 = char_list.len().try_into().unwrap();
+        length >= self.min && length <= self.max
     }
 }
 
 impl fmt::Display for Password {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}", self.text, self.rule)
+        write!(
+            f,
+            "{}: {}-{} {}",
+            self.text, self.min, self.max, self.letter
+        )
     }
 }
 
 fn main() {
     if let Some(lines) = read_lines("input.txt") {
-        for line in lines {
-            if let Some(password) = Password::new(&line) {
-                println!("{}", password)
-            }
-        }
+        let valid_pws: Vec<Password> = lines
+            .iter()
+            .filter_map(|line| Password::new(&line))
+            .filter(|pw| pw.valid_part_1())
+            .collect();
+        println!("There are {} valid passwords", valid_pws.len());
     }
 }
 
