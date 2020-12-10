4a7b87e911e71f5568c0b74b733ea7f3792dcfa7
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-09 23:37:27 -0500

    day-3 part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,9 +1,8 @@
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Lines};
-use std::iter::FilterMap;
+use std::io::{self, BufRead};
 use std::path::Path;
 
-#[derive(std::cmp::PartialEq)]
+#[derive(std::cmp::PartialEq, Debug)]
 enum Square {
     Open,
     Tree,
@@ -33,7 +32,7 @@ impl Map {
         for line in lines {
             for (j, c) in line
                 .chars()
-                .filter_map(|x| Square::from_char(x))
+                .filter_map(Square::from_char)
                 .enumerate()
             {
                 match map.get_mut(j) {
@@ -49,16 +48,29 @@ impl Map {
         self.map.iter().cycle()
     }
 
-    fn navigate(&self, slope: (i8, i8)) -> (i16, i16, i16) {
-        (0, 0, 0)
+    fn navigate(&self, slope: (usize, usize)) -> (usize, usize) {
+        let (rise, run) = slope;
+        let path = self
+            .iter()
+            .step_by(run)
+            .zip((0..).step_by(rise))
+            .map(|(column, height)| column.get(height))
+            .take_while(|op| op.is_some());
+        let (trees, empty): (Vec<Option<&Square>>, Vec<Option<&Square>>) =
+            path.partition(|&x| x.map_or(false, Square::is_tree));
+        let num_trees = trees.len();
+        (num_trees, num_trees + empty.len())
     }
 }
 
 fn main() {
     if let Ok(lines) = read_lines("input.txt") {
         let map = Map::from_lines(lines);
-        let (trees, down, right) = map.navigate((1, 3));
-        println!("PART 1\n{} trees were hit.\nYou went {} squares down the hill.\nYou went {} squares right.", trees, down, right)
+        let (trees, down) = map.navigate((1, 3));
+        println!(
+            "PART 1\n{} trees were hit.\nYou went {} squares down the hill.",
+            trees, down
+        )
     }
 }
 
