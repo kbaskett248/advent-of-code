e107f47c9247442cf7a60f0e9a8225982a1129ae
Kenny Baskett <kbaskett248@gmail.com>
2020-12-10 18:36:16 -0500

    day-4 WIP

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("input.txt") {
+        
+    }
+}
+
+fn chunk_lines(lines: I) -> I
+where 
+    I: impl Iterator<Item = String> {
+    lines.scan(vec![], )
+}
+
+fn read_lines<P>(filename: P) -> io::Result<impl Iterator<Item = String>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines().filter_map(|x| x.ok()))
 }
