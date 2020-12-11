4f7a9f2d02157d1c06ca17f6249fcc6602bb94ae
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-10 21:37:35 -0500

    day-4 pt1

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -5,11 +5,19 @@ use std::path::Path;
 
 fn main() {
     if let Ok(lines) = read_lines("input.txt") {
-        let num_valid_passports = chunk_lines(lines)
+        let passports: Vec<HashMap<String, String>> = chunk_lines(lines)
             .map(parse_passport)
+            .collect();
+
+        let num_valid_passports = passports.iter()
             .filter(|x| passport_is_valid(x))
             .count();
-        println!("PART 1: There are {} valid passports.", num_valid_passports)
+        println!("PART 1: There are {} valid passports.", num_valid_passports);
+
+        let num_valid_passports_pt_2 = passports.iter()
+            .filter(|x| passport_is_valid_pt_2(x))
+            .count();
+        println!("PART 2: There are {} valid passports.", num_valid_passports_pt_2);
     }
 }
 
@@ -51,4 +59,39 @@ fn passport_is_valid(passport: &HashMap<String, String>) -> bool {
     ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
         .iter()
         .all(|k| passport.contains_key(&k.to_string()))
+}
+
+fn passport_is_valid_pt_2(passport: &HashMap<String, String>) -> bool {
+    if passport_is_valid(passport) {
+        passport.iter()
+            .all(|(key, val)| {
+                match key.as_str() {
+                    "byr" => {
+                        if let Ok(num) = val.parse::<i16>() {
+                            num >= 1920 && num <= 2002
+                        } else {
+                            false
+                        }
+                    },
+                    "iyr" => {
+                        if let Ok(num) = val.parse::<i16>() {
+                            num >= 2010 && num <= 2020
+                        } else {
+                            false
+                        }
+                    },
+                    "eyr" => {
+                        if let Ok(num) = val.parse::<i16>() {
+                            num >= 2020 && num <= 2030
+                        } else {
+                            false
+                        }
+                    },
+                    _ => true,
+                    
+                }
+            })
+    } else {
+        false
+    }
 }
