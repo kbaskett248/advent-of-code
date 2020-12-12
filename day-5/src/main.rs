052839448d26e724db9a0fe0a06ff30ccb6aff7a
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-11 20:49:00 -0500

    day-5 part 1

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -5,44 +5,68 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::str::FromStr;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(read_lines("input.txt").expect("read_lines failed")),
+            880
+        );
+    }
+}
+
 fn main() {
     if let Ok(lines) = read_lines("input.txt") {
         println!("The max seat ID is {}.", part_1(lines));
     }
 }
 
-fn part_1(lines: impl Iterator<Item = String>) -> i16 {
-    lines
-        .map(|l| (l[0..7], l[7..]))
-        .
-
-    0
+fn part_1(lines: impl Iterator<Item = String>) -> i32 {
+    if let Some(max) = lines
+        .filter_map(|l| l.parse::<Passport>().ok())
+        .map(|p| p.id())
+        .max()
+    {
+        max
+    } else {
+        0
+    }
 }
 
+#[derive(Debug)]
 struct Passport {
     rep: String,
-    row: i8,
-    seat: i8,
+    row: i32,
+    seat: i32,
+}
+
+impl Passport {
+    fn id(&self) -> i32 {
+        (self.row * 8 + self.seat).into()
+    }
 }
 
 impl FromStr for Passport {
     type Err = PassportError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s[0..7].chars()
-            .map(|c| {
-                match c {
-                    'F' => Ok(0),
-                    'B' => 1,
-                    _ => PassportError
-                }
-            });
-
-        Ok(Passport{
+        let row: i32 = bin_str_to_int(s[..7].replace('F', "0").replace('B', "1"))?;
+        let seat: i32 = bin_str_to_int(s[7..].replace('L', "0").replace('R', "1"))?;
+        Ok(Passport {
             rep: s.to_string(),
-            row: 0,
-            seat: 0
+            row,
+            seat,
         })
+    }
+}
+
+fn bin_str_to_int(s: String) -> Result<i32, PassportError> {
+    match i32::from_str_radix(&s, 2) {
+        Err(_) => Err(PassportError),
+        Ok(num) => Ok(num),
     }
 }
 
