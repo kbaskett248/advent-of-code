a71ca13b73a601d8fedd6159ee1721da2199839a
Kenny Baskett <kbaskett248@gmail.com>
2020-12-13 18:41:12 -0500

    day-9 start

---
+++ day-9/src/main.rs
@@ -0,0 +1,33 @@
+use mylib::read_lines;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(read_lines("input.txt").expect("read_lines failed")),
+            ()
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(read_lines("input.txt").expect("read_lines failed")),
+            ()
+        );
+    }
+}
+
+fn main() {
+    let p1 = part_1(read_lines("input.txt").expect("read_lines failed"));
+    println!("PART 1: {:?}", p1);
+    let p2 = part_2(read_lines("input.txt").expect("read_lines failed"));
+    println!("PART 2: {:?}", p2);
+}
+
+fn part_1(lines: impl Iterator<Item = String>) {}
+
+fn part_2(lines: impl Iterator<Item = String>) {}
