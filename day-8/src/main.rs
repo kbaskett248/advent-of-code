e4a9f3d684e21a5e2ec19e601fe075147b194e4d
Kenny Baskett <kbaskett248@gmail.com>
2020-12-13 13:48:13 -0500

    Move types into separate file

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,8 +1,6 @@
-use std::error::Error;
-use std::fmt;
-use std::str::FromStr;
-
 use mylib::read_lines;
+
+mod types;
 
 #[cfg(test)]
 mod tests {
@@ -33,10 +31,10 @@ fn main() {
 }
 
 fn part_1(lines: impl Iterator<Item = String>) -> i16 {
-    let instructions: Vec<Instruction> = lines
-        .filter_map(|line| line.parse::<Instruction>().ok())
+    let instructions: Vec<types::Instruction> = lines
+        .filter_map(|line| line.parse::<types::Instruction>().ok())
         .collect();
-    let program = Program::new(instructions);
+    let program = types::Program::new(instructions);
     if let Some((state, _)) = program.take_while(|(_, instruction)| instruction.count() == &0).last() {
         state.acc
     } else {
@@ -46,122 +44,3 @@ fn part_1(lines: impl Iterator<Item = String>) -> i16 {
 }
 
 fn part_2(lines: impl Iterator<Item = String>) {}
-
-#[derive(Copy, Clone)]
-enum Instruction {
-    Acc { value: i16, count: i8 },
-    Jmp { value: i16, count: i8 },
-    Nop { count: i8 },
-}
-
-impl Instruction {
-    fn execute(&self, state: &State) -> State {
-        match self {
-            Instruction::Acc { value, .. } => State {
-                acc: state.acc + value,
-                inst: state.inst + 1,
-            },
-            Instruction::Jmp { value, .. } => State {
-                acc: state.acc,
-                inst: state.inst + value,
-            },
-            Instruction::Nop { .. } => State {
-                acc: state.acc,
-                inst: state.inst + 1,
-            },
-        }
-    }
-
-    fn increment_count(&self) -> Instruction {
-        match self {
-            Instruction::Acc { count, value } => Instruction::Acc {
-                count: count + 1,
-                value: *value,
-            },
-            Instruction::Jmp { count, value } => Instruction::Jmp {
-                count: count + 1,
-                value: *value,
-            },
-            Instruction::Nop { count } => Instruction::Nop { count: count + 1 },
-        }
-    }
-
-    fn count(&self) -> &i8 {
-        match self {
-            Instruction::Acc{count, ..} => count,
-            Instruction::Jmp{count, ..} => count,
-            Instruction::Nop{count, ..} => count,
-        }
-    }
-}
-
-#[derive(Copy, Clone)]
-struct State {
-    acc: i16,
-    inst: i16,
-}
-
-impl State {
-    fn new() -> State {
-        State { acc: 0, inst: 0 }
-    }
-}
-
-impl FromStr for Instruction {
-    type Err = InstructionParseError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s.split(' ');
-        let ins = parts.next().ok_or(InstructionParseError)?.trim();
-        let value = parts
-            .next()
-            .ok_or(InstructionParseError)?
-            .trim()
-            .parse::<i16>()
-            .map_err(|_| InstructionParseError)?;
-        match ins {
-            "acc" => Ok(Instruction::Acc { value, count: 0 }),
-            "jmp" => Ok(Instruction::Jmp { value, count: 0 }),
-            "nop" => Ok(Instruction::Nop { count: 0 }),
-            _ => Err(InstructionParseError),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct InstructionParseError;
-impl fmt::Display for InstructionParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Instruction parse error")
-    }
-}
-impl Error for InstructionParseError {}
-
-struct Program {
-    instructions: Vec<Instruction>,
-    state: State,
-}
-
-impl Program {
-    fn new(instructions: Vec<Instruction>) -> Program {
-        let state = State::new();
-        Program {
-            instructions,
-            state,
-        }
-    }
-}
-
-impl Iterator for Program {
-    type Item = (State, Instruction);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let state = self.state;
-        let instruction = *self.instructions.get(self.state.inst as usize)?;
-        let new_state = instruction.execute(&state);
-        let new_instruction = instruction.increment_count();
-        self.instructions[state.inst as usize] = new_instruction;
-        self.state = new_state;
-        Some((state, instruction))
-    }
-}
---
+++ day-8/src/types.rs
@@ -0,0 +1,122 @@
+use std::error::Error;
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Copy, Clone)]
+pub enum Instruction {
+    Acc { value: i16, count: i8 },
+    Jmp { value: i16, count: i8 },
+    Nop { count: i8 },
+}
+
+impl Instruction {
+    fn execute(&self, state: &State) -> State {
+        match self {
+            Instruction::Acc { value, .. } => State {
+                acc: state.acc + value,
+                inst: state.inst + 1,
+            },
+            Instruction::Jmp { value, .. } => State {
+                acc: state.acc,
+                inst: state.inst + value,
+            },
+            Instruction::Nop { .. } => State {
+                acc: state.acc,
+                inst: state.inst + 1,
+            },
+        }
+    }
+
+    fn increment_count(&self) -> Instruction {
+        match self {
+            Instruction::Acc { count, value } => Instruction::Acc {
+                count: count + 1,
+                value: *value,
+            },
+            Instruction::Jmp { count, value } => Instruction::Jmp {
+                count: count + 1,
+                value: *value,
+            },
+            Instruction::Nop { count } => Instruction::Nop { count: count + 1 },
+        }
+    }
+
+    pub fn count(&self) -> &i8 {
+        match self {
+            Instruction::Acc{count, ..} => count,
+            Instruction::Jmp{count, ..} => count,
+            Instruction::Nop{count, ..} => count,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct State {
+    pub acc: i16,
+    inst: i16,
+}
+
+impl State {
+    fn new() -> State {
+        State { acc: 0, inst: 0 }
+    }
+}
+
+impl FromStr for Instruction {
+    type Err = InstructionParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.split(' ');
+        let ins = parts.next().ok_or(InstructionParseError)?.trim();
+        let value = parts
+            .next()
+            .ok_or(InstructionParseError)?
+            .trim()
+            .parse::<i16>()
+            .map_err(|_| InstructionParseError)?;
+        match ins {
+            "acc" => Ok(Instruction::Acc { value, count: 0 }),
+            "jmp" => Ok(Instruction::Jmp { value, count: 0 }),
+            "nop" => Ok(Instruction::Nop { count: 0 }),
+            _ => Err(InstructionParseError),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct InstructionParseError;
+impl fmt::Display for InstructionParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Instruction parse error")
+    }
+}
+impl Error for InstructionParseError {}
+
+pub struct Program {
+    instructions: Vec<Instruction>,
+    state: State,
+}
+
+impl Program {
+    pub fn new(instructions: Vec<Instruction>) -> Program {
+        let state = State::new();
+        Program {
+            instructions,
+            state,
+        }
+    }
+}
+
+impl Iterator for Program {
+    type Item = (State, Instruction);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let state = self.state;
+        let instruction = *self.instructions.get(self.state.inst as usize)?;
+        let new_state = instruction.execute(&state);
+        let new_instruction = instruction.increment_count();
+        self.instructions[state.inst as usize] = new_instruction;
+        self.state = new_state;
+        Some((state, instruction))
+    }
+}
