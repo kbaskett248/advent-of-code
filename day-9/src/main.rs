8127273d9f71508211fc3dd7018032dfb47a3b21
Kenneth Baskett <kbaskett248@gmail.com>
2020-12-13 23:28:30 -0500

    day 9 part 1 WIP

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,3 +1,5 @@
+use std::iter::{repeat, repeat_with};
+
 use mylib::read_lines;
 
 #[cfg(test)]
@@ -8,7 +10,7 @@ mod tests {
     fn test_part_1() {
         assert_eq!(
             part_1(read_lines("input.txt").expect("read_lines failed")),
-            ()
+            0
         );
     }
 
@@ -17,6 +19,27 @@ mod tests {
         assert_eq!(
             part_2(read_lines("input.txt").expect("read_lines failed")),
             ()
+        );
+    }
+
+    #[test]
+    fn test_combinations_2() {
+        let c: Vec<(u64, u64)> = combinations(&[1, 2]).collect();
+        assert_eq!(c.as_slice(), [(1, 2)]);
+    }
+
+    #[test]
+    fn test_combinations_3() {
+        let c: Vec<(u64, u64)> = combinations(&[1, 2, 3]).collect();
+        assert_eq!(c.as_slice(), [(1, 2), (1, 3), (2, 3)]);
+    }
+
+    #[test]
+    fn test_combinations_4() {
+        let c: Vec<(u64, u64)> = combinations(&[1, 2, 3, 4]).collect();
+        assert_eq!(
+            c.as_slice(),
+            [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
         );
     }
 }
@@ -28,6 +51,41 @@ fn main() {
     println!("PART 2: {:?}", p2);
 }
 
-fn part_1(lines: impl Iterator<Item = String>) {}
+fn part_1(lines: impl Iterator<Item = String>) -> u64 {
+    let numbers: Vec<u64> = to_numbers(lines).collect();
+    if let Some((_, num)) = repeat(numbers)
+        .enumerate()
+        .skip(25)
+        .flat_map(|(index, nums)| {
+            let num = nums[index];
+            nums[(index - 26)..(index - 1)].iter().map(move |n| (num, *n))
+        })
+        .find(|(n, nums)| is_sum(*n, nums)) {
+            num.clone()
+        } else {
+            0
+        }
+    0
+}
+
+fn is_sum(num: u64, preceding: &'static [u64]) -> bool {
+    combinations(preceding)
+        .find(|(a, b)| a + b == num)
+        .is_some()
+}
+
+fn combinations(numbers: &'static [u64]) -> impl Iterator<Item = (u64, u64)> {
+    repeat(numbers)
+        .enumerate()
+        .take(numbers.len() - 1)
+        .flat_map(|(index, nums)| {
+            let num = nums[index];
+            nums[(index + 1)..].iter().map(move |n| (num, *n))
+        })
+}
 
 fn part_2(lines: impl Iterator<Item = String>) {}
+
+fn to_numbers(lines: impl Iterator<Item = String>) -> impl Iterator<Item = u64> {
+    lines.filter_map(|line| line.parse::<u64>().ok())
+}
