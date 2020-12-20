74f31935ff13791c75b2491e7052f31cb71808aa
Kenny Baskett <kbaskett248@gmail.com>
2020-12-20 17:22:31 -0500

    11.2 complete

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,3 +1,5 @@
+// https://adventofcode.com/2020/day/11
+
 use std::cmp::{min, max};
 use mylib::read_lines;
 use std::time::Instant;
@@ -28,7 +30,15 @@ mod tests {
     fn test_part_2() {
         assert_eq!(
             part_2(read_lines("input.txt").expect("read_lines failed")),
-            ()
+            1865
+        );
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(
+            part_2(read_lines("p1.example.txt").expect("read_lines failed")),
+            26
         );
     }
 }
@@ -71,8 +81,53 @@ fn part_1(lines: impl Iterator<Item = String>) -> usize {
     while current != *chart.peek().expect("Failed to return item") {
         current = chart.next().expect("Failed to return item");
     }
-    println!("{}", current.to_string());
+    // println!("{}", current.to_string());
     current.count_occupied()
 }
 
-fn part_2(lines: impl Iterator<Item = String>) {}
+// Given the new visibility method and the rule change for occupied seats becoming empty, 
+// once equilibrium is reached, how many seats end up occupied?
+fn part_2(lines: impl Iterator<Item = String>) -> usize {
+    fn neighbors(seats: &Vec<Vec<types::Tile>>, row: usize, col: usize) -> Vec<types::Tile> {
+        let modifiers: Vec<fn(i8) -> i8> = vec![|n| n-1, |n| n, |n| n+1];
+
+        let r_max = (seats.len() - 1) as i8;
+        let c_max = (seats[row].len() - 1) as i8;
+
+        let mut tiles = Vec::with_capacity(8);
+
+        for rm in &modifiers {
+            for cm in &modifiers {
+                let mut r = rm(row as i8);
+                let mut c = cm(col as i8);
+
+                if (r, c) == (row as i8, col as i8) {
+                    continue
+                }
+
+                while r >= 0 && r <= r_max && c >= 0 && c <= c_max {
+                    match seats[r as usize][c as usize] {
+                        types::Tile::Floor => {
+                            r = rm(r);
+                            c = cm(c);
+                        },
+                        seat @ types::Tile::Seat{..} => {
+                            tiles.push(seat);
+                            break
+                        }
+                    }
+                }
+            }
+        }
+
+        tiles
+    }
+    
+    let mut chart = types::SeatingChart::from_lines(lines, 5, neighbors).peekable();
+    let mut current = chart.next().expect("No seating chart");
+    while current != *chart.peek().expect("Failed to return item") {
+        current = chart.next().expect("Failed to return item");
+    }
+    // println!("{}", current.to_string());
+    current.count_occupied()
+}
