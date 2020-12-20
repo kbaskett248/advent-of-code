fb34e4dda76206e81bba7819f062f5e4d707737f
Kenny Baskett <kbaskett248@gmail.com>
2020-12-20 18:10:24 -0500

    day 12 start

---
+++ day-12/src/main.rs
@@ -0,0 +1,48 @@
+// https://adventofcode.com/2020/day/12
+
+use mylib::read_lines;
+use std::time::Instant;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_example() {
+        assert_eq!(
+            part_1(read_lines("p1.example.txt").expect("read_lines failed")),
+            25
+        );
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(read_lines("input.txt").expect("read_lines failed")),
+            ()
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(read_lines("input.txt").expect("read_lines failed")),
+            ()
+        );
+    }
+}
+
+fn main() {
+    let mut start = Instant::now();
+    let p1 = part_1(read_lines("input.txt").expect("read_lines failed"));
+    println!("PART 1: {:?} ({:?})", p1, start.elapsed());
+
+    start = Instant::now();
+    let p2 = part_2(read_lines("input.txt").expect("read_lines failed"));
+    println!("PART 2: {:?} ({:?})", p2, start.elapsed());
+}
+
+// What is the Manhattan distance between that location and the ship's starting position?
+fn part_1(lines: impl Iterator<Item = String>) {}
+
+fn part_2(lines: impl Iterator<Item = String>) {}
