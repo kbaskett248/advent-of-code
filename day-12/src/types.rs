006e6dfa25e29e50969df0bf8ab61f43b69153a5
Kenny Baskett <kbaskett248@gmail.com>
2020-12-21 06:38:16 -0500

    day-12 move function

--- day-12/src/types.rs
+++ day-12/src/types.rs
@@ -5,15 +5,15 @@ use std::fmt;
 use std::fmt::{Debug, Display};
 use std::str::FromStr;
 
+fn to_rotation_matrix(angle: f32) -> Array2<f32> {
+    let radians = (angle as f32) / 180.0 * PI;
+    arr2(&[[radians.cos(), -radians.sin()], [radians.sin(), radians.cos()]])
+}
+
 pub enum Action {
     Rotate(Array2<f32>),
     Shift(Array2<f32>),
     Forward(f32),
-}
-
-fn to_rotation_matrix(angle: f32) -> Array2<f32> {
-    let radians = (angle as f32) / 180.0 * PI;
-    arr2(&[[radians.cos(), -radians.sin()], [radians.sin(), radians.cos()]])
 }
 
 impl FromStr for Action {
@@ -50,3 +50,18 @@ impl fmt::Display for ActionParseError {
     }
 }
 impl Error for ActionParseError {}
+
+pub struct Boat {
+    direction: Array2<f32>,
+    location: Array2<f32>,
+}
+
+impl Boat {
+    pub fn apply_action(&mut self, action: Action) {
+        match action {
+            Action::Rotate(m) => self.direction = m * &self.direction,
+            Action::Shift(s) => self.location = s * &self.direction,
+            Action::Forward(m) => self.location = &self.location + &(m * &self.direction),
+        }
+    }
+}
