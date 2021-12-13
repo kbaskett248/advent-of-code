a83c90a411670bc44de83086589a496c91e212e4
Kenny Baskett <kbaskett248@gmail.com>
2021-12-12 21:29:22 -0500

    WI: Day 8 pt 2

--- 2021/day-8/src/types.rs
+++ 2021/day-8/src/types.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
 use std::fmt::{Debug, Display};
@@ -41,3 +42,47 @@ impl Display for EntryParseError {
     }
 }
 impl Error for EntryParseError {}
+
+pub struct MappedEntry {
+    pub digit_mapping: HashMap<String, u8>,
+    pub digits: Vec<String>,
+}
+
+fn sort_string(s: &String) -> String {
+    let mut sorted_digit = s.chars().collect::<Vec<char>>();
+    sorted_digit.sort_unstable();
+    String::from_iter(sorted_digit)
+}
+
+// 5: 2, 3, 5,
+// 6: 0, 6, 9
+
+impl From<Entry> for MappedEntry {
+    fn from(entry: Entry) -> MappedEntry {
+        let mut digit_mapping: HashMap<String, u8> = HashMap::with_capacity(10);
+        let mut remaining_patterns: Vec<String> = Vec::with_capacity(6);
+        for pattern in entry.patterns {
+            let sorted_pattern = sort_string(&pattern);
+            match sorted_pattern.len() {
+                2 => {
+                    digit_mapping.insert(sorted_pattern, 1);
+                }
+                3 => {
+                    digit_mapping.insert(sorted_pattern, 7);
+                }
+                4 => {
+                    digit_mapping.insert(sorted_pattern, 4);
+                }
+                7 => {
+                    digit_mapping.insert(sorted_pattern, 8);
+                }
+                _ => remaining_patterns.push(sorted_pattern),
+            };
+        }
+        for pattern in remaining_patterns {}
+        MappedEntry {
+            digit_mapping,
+            digits: entry.digits,
+        }
+    }
+}
