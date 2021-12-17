c2645502ef98bf0234bc99b0dce01c71460a0809
Kenneth Baskett <kbaskett248@gmail.com>
2021-12-16 22:14:19 -0500

    Day 4 init

---
+++ 2021/day-4/src/main.rs
@@ -0,0 +1,53 @@
+use mylib::read_lines;
+use std::time::Instant;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_example() {
+        assert_eq!(
+            part_1(read_lines("example.txt").expect("read_lines failed")),
+            4512
+        );
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(read_lines("input.txt").expect("read_lines failed")),
+            0
+        );
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        assert_eq!(
+            part_2(read_lines("example.txt").expect("read_lines failed")),
+            ()
+        );
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(read_lines("input.txt").expect("read_lines failed")),
+            ()
+        );
+    }
+}
+
+fn main() {
+    let mut start = Instant::now();
+    let p1 = part_1(read_lines("input.txt").expect("read_lines failed"));
+    println!("PART 1: {:?} ({:?})", p1, start.elapsed());
+
+    start = Instant::now();
+    let p2 = part_2(read_lines("input.txt").expect("read_lines failed"));
+    println!("PART 2: {:?} ({:?})", p2, start.elapsed());
+}
+
+fn part_1(lines: impl Iterator<Item = String>) {}
+
+fn part_2(lines: impl Iterator<Item = String>) {}
